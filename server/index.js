4abe02bc961cdb65a31c40a7bb942aef7b95a48d
neetugond <ngond92@gmail.com>
2023-05-23 00:00:01 +0530

    post route, schema and controller created

--- server/index.js
+++ server/index.js
@@ -10,9 +10,11 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { register } from './controllers/auth.js';
 import  authRoutes from './routes/auth.js'
 import userRoutes from "./routes/users.js" 
+import postRoutes from './routes/posts.js'
+import { register } from './controllers/auth.js';
+import {createPost} from './controllers/posts.js'
 import { verifyToken } from './middleware/auth.js';
 
 // configuration 
@@ -51,9 +53,11 @@ const upload = multer({ storage })
 
 // routes with files
 app.post("/auth/register", upload.single("picture"), register);
+app.post('/posts', verifyToken, upload.single("picture"), createPost);
 
 app.use('/auth', authRoutes);
 app.use('/users', userRoutes);
+app.use('/posts', postRoutes)
 // mongoose setup
 const PORT = process.env.PORT || 6000;
 mongoose.connect(process.env.MONGO_URL, {
