ff7f26160cb0abe8885717c89b280116d01b6489
neetugond <ngond92@gmail.com>
2023-05-22 22:05:55 +0530

    auth controller created and brcypt the password, saved user and send data back to the frontend

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,3 +1,47 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
+
+// register user
+
+// api call from frontend to backend and backenf to database
+// req = this will provide request body from frontend
+// res = response will going to send back tp the frontend
+export const register = async (req, res) => {
+    try {
+        const {
+            firstName,
+            lastName,
+            email,
+            password,
+            picturePath,
+            friends,
+            location,
+            occupation,
+        } = req.body;
+
+        // bcrypt the password
+        const salt = await bcrypt.genSalt();
+        const passwordHash = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            // same naming convention key value that' why only stored only one name
+            firstName,
+            lastName,
+            email,
+            password: passwordHash,
+            picturePath,
+            friends,
+            location,
+            occupation,
+            // giving some random dummy value
+            viewedProfile: Math.floor(Math.random() * 10000),
+            impressions: Math.floor(Math.random() * 10000)
+        });
+        const savedUser = await newUser.save();
+        // after save data from can receive this response
+        res.status(201).json(savedUser);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
