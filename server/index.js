86310d4c87091405d4e99b2f85a8e1bce9a8622b
neetugond <ngond92@gmail.com>
2023-05-22 20:51:42 +0530

    configuration

--- server/index.js
+++ server/index.js
@@ -1,1 +1,32 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+// middleware for handling multipart/form-data, which is primarily used for uploading files
+import multer from 'multer';
+// Helmet helps secure Express apps by setting HTTP response headers.
+import helmet from 'helmet';
+import morgan from 'morgan';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// configuration 
+// when type is module we can access filepath in this way
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename)
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
+
+
+
 
